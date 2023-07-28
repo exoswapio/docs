5e7f95825a0cf6d9c8de575a553c3c10509648eb
weichen-qsnetwork <weichen@thestratos.org>
2023-07-27 22:16:01 -0400

    Add CNAME file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,8 +18,9 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ExoSwap', // Usually your GitHub org/user name.
-  projectName: 'exoswap-docs', // Usually your repo name.
+  organizationName: 'exoswapio', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
